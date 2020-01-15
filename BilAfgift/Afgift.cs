66ccd2262d94df9a0be6674471a989e851cdd455
Gitte Sevel Bilenberg <36244161+GitteSB@users.noreply.github.com>
2020-01-15 09:34:07 +0100

    Add files via upload

---
+++ BilAfgift/Afgift.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BilAfgift
+{
+    public class Afgift
+    {
+        
+        ///<summary>Returnerer afgiften for en almindelig personbil ud fra dens pris.
+        /// <para>Afgiften bliver rundet af til nærmeste tal uden decimal</para>
+        /// </summary>
+        public int BilAfgift(int pris)
+        {
+            if (pris <= 0)
+            {
+                throw new ArgumentException("Prisen skal være højere end 0.");
+            }
+
+            if (pris <= 200000)
+            {
+                return Convert.ToInt32(Math.Round(pris * 0.85));
+            }
+
+            return Convert.ToInt32(Math.Round(pris * 1.50 - 130000));
+        }
+
+        ///<summary>Returnerer afgiften for en elektrisk personbil ud fra dens pris.
+        /// <para>Afgiften bliver udregnet ud fra 20% af en almindelig personbils med tilsvarende pris' afgift </para>
+        /// <para>Afgiften bliver rundet af til nærmeste tal uden decimal</para>
+        /// </summary>
+        public int ElBilAfgift(int pris)
+        {
+            int bilAfgift = BilAfgift(pris);
+            return Convert.ToInt32(Math.Round(bilAfgift * 0.2));
+        }
+    }
+}
